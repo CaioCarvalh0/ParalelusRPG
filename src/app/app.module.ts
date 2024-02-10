c069701915875982fa972ac1c69afeebe87ef9b2
Caio de Carvalho Oliveira <yinz.desenho@gmail.com>
2024-02-10 02:08:05 -0300

    Primeiro Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './forms/login/login.component';
+import { CadastroComponent } from './forms/cadastro/cadastro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
